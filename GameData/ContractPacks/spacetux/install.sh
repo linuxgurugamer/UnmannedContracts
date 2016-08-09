64dc006f579fc37f7b6dc8207c0c97255b803729
Jonathan Bayer <linuxgurugamer@gmail.com>
2016-08-09 07:27:12 -0400

    Added checks for impossible (in-game) Kolniya orbits Added Tundra orbits

--- GameData/ContractPacks/spacetux/install.sh
+++ GameData/ContractPacks/spacetux/install.sh
@@ -4,7 +4,7 @@ ROOT=/d/Users/jbb
 
 INSTALLDIR=$(ROOT)/install
 MODDIR=GameData/ContractPacks/Spacetux/UnmannedContracts
-OBJECTS=CHANGELOG-UnmannedContracts License-UnmannedContracts.txt UnmannedContracts.version coherentcontractsmod.cfg UnmannedContracts.dll group.cfg  UnmannedContracts.cfg
+OBJECTS=CHANGELOG-UnmannedContracts License-UnmannedContracts.txt UnmannedContracts.version coherentcontractsmod.cfg UnmannedContracts.dll group.cfg  UnmannedContracts.cfg group.cfg
 
 AGENCY=spacetux
 
